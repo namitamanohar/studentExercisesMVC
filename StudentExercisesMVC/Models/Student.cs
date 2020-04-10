863cab927a48d17876e4032f9e8ff879733479da
namitamanohar <manohar.namita@gmail.com>
2020-04-10 16:27:01 -0500

    Display names on models view models

--- StudentExercisesMVC/Models/Student.cs
+++ StudentExercisesMVC/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,14 +10,18 @@ namespace StudentExercisesMVC.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+        [Display(Name = "Slack Handle")]
         public string SlackHandle { get; set; }
 
         public int CohortId { get; set; }
 
+        [Display(Name = "Cohort Name")]
         public Cohort cohort { get; set; }
 
     }
--- StudentExercisesMVC/Models/ViewModels/StudentEditViewModel.cs
+++ StudentExercisesMVC/Models/ViewModels/StudentEditViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,13 +10,14 @@ namespace StudentExercisesMVC.Models.ViewModels
     public class StudentEditViewModel
     {
         public int StudentId { get; set; }
-
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
-
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
-
+        [Display(Name = "Slack Handle")]
         public string SlackHandle { get; set; }
 
+        [Display (Name ="Cohort Name")]
         public int CohortId { get; set; }
 
         public List<SelectListItem> CohortOptions { get; set; }
